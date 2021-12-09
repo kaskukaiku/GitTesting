875f052a971e71b49e1d5f2f4e0e961da14411e2
Kasperi Laine <kasperi.laine@gmail.com>
2021-12-09 18:43:26 +0200

    Adding the deduction function

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def add(number1, number2):
     return resultadd
 
 add(3,6)
+
+def deduction(number1, number2):
+    resultdeduction = number1 - number2
+    return resultdeduction
+
+deduction(10,6)
